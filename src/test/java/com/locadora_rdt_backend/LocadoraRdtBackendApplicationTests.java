48e99ed7f8c125b1e66405fdd6991846b82ca9f8
Renan Duarte Gomes <renandg.axl@gmail.com>
2026-06-10 14:35:08 -0300

    Docker and Docker Compose deployed in the project.

--- src/test/java/com/locadora_rdt_backend/LocadoraRdtBackendApplicationTests.java
+++ src/test/java/com/locadora_rdt_backend/LocadoraRdtBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.locadora_rdt_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LocadoraRdtBackendApplicationTests {
 
 	@Test
