c60c4fcde71b3d3dd7c480f5ad778ec42344e93a
Renan Duarte Gomes <renandg.axl@gmail.com>
2026-07-16 14:10:28 -0300

    Pipeline main test 02

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -466,3 +466,4 @@ INSERT INTO tb_rental_status_history (
       (0, (SELECT id FROM tb_rental WHERE rental_number = 'LOC-20260007'), 'PENDING', 'CANCELLED', 'Cancelamento solicitado pelo cliente.', '2026-07-04T12:00:00Z', 'admin', CURRENT_TIMESTAMP, NULL, 'admin', NULL);
 
 
+
